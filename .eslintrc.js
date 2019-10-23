b7f9a828d5fc595fd7bc6074d77b934038616899
Paulo Humberto <phumbertodasilva@gmail.com>
2019-10-22 23:40:33 -0300

    Arquivos de configuração do node e vscode

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+
+  extends: [
+    'airbnb-base', 'prettier',
+  ],
+
+  plugins: ['prettier'],
+
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+
+  parseOptions: {
+    ecmaVersion: 2018,
+    sourceTeyp: 'module',
+  },
+
+  rules: {
+    "prettier/prettier": "error",
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+    "camelcase": "off",
+    "no_unused-vars": ["error", { "argsIgnorePattern": "next"}],
+  }
+}
