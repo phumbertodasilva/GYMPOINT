37e0e2e1c7c415961bdd802d86f6cd3cf930b245
Paulo Humberto <phumbertodasilva@gmail.com>
2019-10-23 14:41:55 -0300

    Implementado creação e alteração de alunos

---
+++ src/app/controllers/StudentController.js
@@ -0,0 +1,76 @@
+import * as Yup from 'yup';
+import Student from '../models/Student';
+
+class StudentController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      age: Yup.number(),
+      weight: Yup.number(),
+      height: Yup.number(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation false' });
+    }
+
+    const studentExists = await Student.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (studentExists) {
+      return res.status(400).json({ error: 'Student already exists.' });
+    }
+
+    const { id, name, email, age, weight, height } = await Student.create(
+      req.body
+    );
+
+    return res.json({
+      id,
+      name,
+      email,
+      age,
+      weight,
+      height,
+    });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      age: Yup.number(),
+      weight: Yup.number().positive(),
+      height: Yup.number().positive(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation false' });
+    }
+
+    const student = await Student.findOne({ where: { email: req.body.email } });
+
+    if (!student){
+      return res.status(400).json({ error: 'Student not found.' });
+    }
+
+    const { id, name, email, age, weight, height } = await student.update(
+      req.body
+    );
+
+    return res.json({
+      id,
+      name,
+      email,
+      age,
+      weight,
+      height,
+    });
+  }
+}
+
+export default new StudentController();
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import SessionController from './app/controllers/SessionController';
+import StudentController from './app/controllers/StudentController';
 
 import authMiddleware from './app/middleware/auth';
 
@@ -8,5 +9,8 @@ const routes = new Router();
 
 routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
+
+routes.post('/students', StudentController.store);
+routes.put('/students', StudentController.update);
 
 export default routes;
