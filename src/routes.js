6965fd25e61cd085fe67d6485553b8145d595431
Paulo Humberto <phumbertodasilva@gmail.com>
2019-10-23 01:49:55 -0300

    Adicionado JWT, conexão com o banco e Sessão

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,12 @@
 import { Router } from 'express';
 
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middleware/auth';
+
 const routes = new Router();
+
+routes.post('/sessions', SessionController.store);
+routes.use(authMiddleware);
 
 export default routes;
